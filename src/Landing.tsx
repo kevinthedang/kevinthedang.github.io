1081cb051e02a12c383155621741e21223bd27c9
Kevin Dang <kevinthedang_1@outlook.com>
2023-11-22 02:20:44 -0800

    Update: renamed tsx files

---
+++ src/Landing.tsx
@@ -0,0 +1,20 @@
+import './Landing.css'
+
+function App() {
+  return (
+    <>
+      <div className="root-container">
+        <div className="primary-window">
+          {/* Contains primary features like socials, projects, etc */}
+          <h1>Kevin Dang</h1>
+        </div>
+
+        <div className="fun-content">
+          {/* Put fun stuff in the background! */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App

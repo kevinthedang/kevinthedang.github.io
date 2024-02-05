f09fd36302dd83cdd5690d530b047064f78291b4
kevinthedang <kevinthedang_1@outlook.com>
2024-02-04 20:21:25 -0800

    fixed lint and updated packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,19 +7,36 @@ module.exports = {
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react-refresh', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/prefer-nullish-coalescing': 'error'
   },
   overrides: [
     {
+      files: ['**/*.ts', 'vite.config.ts'],
+      rules: {
+        '@typescript-eslint/prefer-nullish-coalescing': 'off'
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',

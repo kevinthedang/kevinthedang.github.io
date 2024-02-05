83b2429a3ae42c4fa2bd0e6146c71f23fd8932a9
kevinthedang <kevinthedang_1@outlook.com>
2024-02-04 20:56:42 -0800

    added version at bottom

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -7,6 +7,9 @@ function App() {
         <div className="primary-window">
           {/* Contains primary features like socials, projects, etc */}
           <h1>Kevin Dang</h1>
+          <div id='footer'>
+            <p id='version'>v0.0.1</p>
+          </div>
         </div>
 
         <div className="fun-content">
